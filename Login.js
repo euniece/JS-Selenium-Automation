2239f526913310017c3106dc3004b3ec2d609545
euniece <euniece.narvades@gmail.com>
2026-02-08 08:29:04 +0800

    Initial commit of Login.js

---
+++ Login.js
@@ -0,0 +1,46 @@
+const {Builder, By, Key, util, until} = require ("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+async function LogIn(params) {
+    let driver = new Builder().forBrowser('chrome').build();
+
+
+    try{
+
+        //Open Website
+        await driver.get("https://www.tutorialspoint.com/selenium/practice/text-box.php");
+        console.log("Website opened successfully");
+
+        //Click the following fields Fullname, email, current address, password Field, button
+
+        //Fullname xpath //*[@id="fullname"]
+        await driver.wait(until.elementLocated(By.id('fullname')),10000)
+        driver.findElement(By.id('fullname')).sendKeys('John Rovic');
+        console.log("Fullname Entered");
+
+        //Email xpath //*[@id="email"]
+        await driver.wait(until.elementLocated(By.xpath('//*[@id="email"]')),10000)
+        driver.findElement(By.xpath('//*[@id="email"]')).sendKeys('test_automation@email.com');
+        console.log("Email Entered");
+
+        //Current Address xpath //*[@id="address"]
+        await driver.wait(until.elementLocated(By.xpath('//*[@id="address"]')),10000)
+        driver.findElement(By.xpath('//*[@id="address"]')).sendKeys('4-2-8 Shibakoen, Minato-ku, Tokyo 105-0011, Japan');
+        console.log("Address Entered");
+        
+        //Password xpath //*[@id="password"]
+        await driver.wait(until.elementLocated(By.id('password')),10000)
+        driver.findElement(By.id('password')).sendKeys('test@Password123');
+        console.log("Password Entered");
+
+        //Click Submit xpath //*[@id="TextForm"]/div[5]/input 
+        //const buttonElement = await driver.findElement(By.css('<button_element_css_selector>')); 
+        const buttonElement = await driver.findElement(By.xpath('//*[@id="TextForm"]/div[5]/input')); 
+        await buttonElement.click();
+        console.log("Button Clicked");
+    } 
+    finally{
+        await driver.sleep(5000);
+        await driver.quit();
+    }
+}
+LogIn();
