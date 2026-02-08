aa081e5007e809576e4e8e7d37351301dc44b2f4
euniece <euniece.narvades@gmail.com>
2026-02-08 11:11:01 +0800

    Update Login.js

--- Login.js
+++ Login.js
@@ -10,8 +10,6 @@ async function LogIn(params) {
         await driver.get("https://www.tutorialspoint.com/selenium/practice/text-box.php");
         console.log("Website opened successfully");
 
-        //Click the following fields Fullname, email, current address, password Field, button
-
         //Fullname xpath //*[@id="fullname"]
         await driver.wait(until.elementLocated(By.id('fullname')),10000)
         driver.findElement(By.id('fullname')).sendKeys('John Rovic');
@@ -34,8 +32,8 @@ async function LogIn(params) {
 
         //Click Submit xpath //*[@id="TextForm"]/div[5]/input 
         //const buttonElement = await driver.findElement(By.css('<button_element_css_selector>')); 
-        const buttonElement = await driver.findElement(By.xpath('//*[@id="TextForm"]/div[5]/input')); 
-        await buttonElement.click();
+        const submitBTN = await driver.findElement(By.xpath('//*[@id="TextForm"]/div[5]/input')); 
+        await submitBTN.click();
         console.log("Button Clicked");
     } 
     finally{
